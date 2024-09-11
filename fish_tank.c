9a510d714ec29f79b52e45259c57fdaafc56fd4d
student <remington.neustadter@atlasschool.com>
2024-09-11 05:46:25 +0000

    For some reason roound functon not working will look at later everything else work.

--- fish_tank.c
+++ fish_tank.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
 #include<math.h>
+#include<stdio.h>
+
 
 
 int main(void)
@@ -19,14 +20,14 @@ int main(void)
 	amount_in_gallons = volume_cubid_inches / 231;
 
 	// show input and thenshow volume
-	printf("The volume of a fish tank having length - %1f width -- %1f and height - %1f is %2f cubic inches\n", length, width, height, volume_cubid_inches);
+	printf("The volume of a fish tank having length - %.1f width -- %.1f and height - %.1f is %.2f cubic inches\n", length, width, height, volume_cubid_inches);
 
 	// show output in gallons now
-	printf("The amount of water needed to fill this tank is - %2f gallons\n", amount_in_gallons);
+	printf("The amount of water needed to fill this tank is - %.2f gallons\n", amount_in_gallons);
 
 	// now lets do some rounding
-	printf("The amount of water needed using the round function is -%2f gallons\n", round(amount_in_gallons));
-
+	///printf("The amount of water needed using the round function is -%.2f gallons\n", round(amount_in_gallons));
+//something wrong with round function
 	return 0;
 
 }
